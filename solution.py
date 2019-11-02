e519e0e92a639f7a1181a2a7a49e70fca88987bb
Mons Erling Mathiesen <mons.e.mathiesen@gmail.com>
2019-11-02 18:33:22 +0000

    Path cost and utils for astar

--- solution.py
+++ solution.py
@@ -6,19 +6,15 @@ Pedro Pinto, Mons Erling Mathiesen, Amanda K. Jansen
 Problem class, Node class, astar_search and best_first_graph_search from https://github.com/aimacode/aima-python/blob/master/search.py
 """
 
-import bisect
 import math
 import random
 import sys
-import utils
-from collections import deque
-"""
-from utils import ( 
+from utils import (
     is_in, argmin, argmax, argmax_random_tie, probability, weighted_sampler,
     memoize, print_table, open_data, PriorityQueue, name,
     distance, vector_add
 )
-"""
+
 infinity = float('inf')
 
 
@@ -34,8 +30,8 @@ class ASARProblem(object):
         state, if there is a unique goal. Your subclass's constructor can add
         other arguments."""
         airports, airplanes, airplane_class, legs = self.load(filename)
-        #Initial state: [[plane[id], startpos, currentpos, timeused], profit, openlist]
-        self.initial = Node([[[plane[0], None, None, 0] for plane in airplanes], [0], legs])
+        #Initial state: [[planeID, planepos, planeitme], profit, openlist of legs]
+        self.initial = Node([[[plane[0], None, 0] for plane in airplanes], [0], legs])
         self.goal = goal
 
     def actions(self, state):
@@ -68,10 +64,33 @@ class ASARProblem(object):
         is such that the path doesn't matter, this function will only look at
         state2.  If the path does matter, it will consider c and maybe state1
         and action. The default method costs 1 for every step in the path."""
-        return c + 1
+        openlist = [['LPPT LPPR', '0055', 'a320', 100, 'a330', 80]] #modify to get openlist from state
+        action = ['LPPT LPPR', 'CS-TUA'] #given
+        airplanes = [['CS-TUA', 'a330'], ['CS-TVA', 'a320']] #need global variable with aircraftclasses
+        ac_class = ''
+        for i in range(len(airplanes)):
+            if action[1] == airplanes[i][0]: #getting class of airplane used in action
+                ac_class = airplanes[i][1]
+        for leg in openlist:
+            if leg[0] == action[0]:
+                cost = 1/leg[leg.index(ac_class)+1] #finding cost for class used in action
+
+        return cost
 
     def heuristic(self, state):
-        """An best case estimation. Must be lower or equal to realistic minimum cost"""
+        """A best case estimation. Must be lower or equal to realistic minimum cost
+
+            Using the inverse of max profit as the heuristic.
+        """
+        min_cost = 0
+        for leg in L:
+            profit = leg[3]
+            for profits in range(3, len(leg), 2):
+                if leg[profits] >= profit:
+                    profit = leg[profits]
+
+            min_cost += 1/profit
+
         raise NotImplementedError
 
     def load(file):  # loads a problem from a file object f
@@ -99,10 +118,10 @@ class ASARProblem(object):
 
 
 airport, aircraft_class, airplane, legs = ASARProblem.load('example.txt')
-print('Airports: ',  airport)
-print('Aircraft class: ', aircraft_class)
-print('Airplanes: ', airplane)
-print('Legs: ', legs)
+# print('Airports: ',  airport)
+# print('Aircraft class: ', aircraft_class)
+# print('Airplanes: ', airplane)
+# print('Legs: ', legs)
 
 # ______________________________________________________________________________
 
@@ -201,3 +220,26 @@ def astar_search(problem, h=None):
     else in your Problem subclass."""
     h = memoize(h or problem.h, 'h')
     return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+
+
+A = [['LPPT', '0600', '2300'],
+     ['LPPR', '0600', '2200'],
+     ['LPFR', '0800', '2000'],
+     ['LPMA', '0800', '2200']]
+
+P = [['CS-TUA', 'a330'],
+     ['CS-TTT', 'a320'],
+     ['CS-TVA', 'a320']]
+
+L = [['LPPT LPPR', '0055', 'a320', 100, 'a330', 80],
+     ['LPPR LPPT', '0055', 'a320', 100, 'a330', 80],
+     ['LPPT LPFR', '0045', 'a320', 80, 'a330', 20],
+     ['LPFR LPPT', '0045', 'a320', 80, 'a330', 20],
+     ['LPPT LPMA', '0145', 'a320', 90, 'a330', 120],
+     ['LPMA LPPT', '0145', 'a320', 90, 'a330', 120]]
+
+C = [['a320', '0045'],
+     ['a330', '0120']]
+
+#state1 = Node([[[plane[0], None, 0] for plane in P], 10, L])
+#print(state1.state[1])
