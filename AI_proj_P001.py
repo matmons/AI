9f6a08b7f252a5b0c542b0f6c19808db8b308a49
PedroPinto42 <56433006+PedroPinto42@users.noreply.github.com>
2019-11-02 17:49:53 +0000

    Update AI_proj_P001.py

--- AI_proj_P001.py
+++ AI_proj_P001.py
@@ -42,25 +42,43 @@ for leg in L:
 print(max_profit)
 
 
-#  initial state creation   
-# [[plane_name, plane_location, plane_time],[profit_so_far],[leg_to_fly1, leg_to_fly2],[schedule]]
+#State structure   
+# [[plane_name, plane_location, plane_time],[profit_so_far],[legs_to_fly],[schedule]
+# example : [[['CS-TUA', 'LPPR', '0715'], ['CS-TTT', None, None], ['CS-TVA', None, None]], 
+#              [80], 
+#              [[ 'LPPT LPPR', '0055', 'a320', 100, 'a330', 80 ], [ 'LPPR LPPT', '0055', 'a320', 100, 'a330', 80 ], [ 'LPPT LPFR', '0045', 'a320', 80, 'a330', 20 ], [ 'LPFR LPPT', '0045', 'a320', 80, 'a330', 20 ], [ 'LPPT LPMA', '0145', 'a320', 90, 'a330', 120 ], [ 'LPMA LPPT', '0145', 'a320', 90, 'a330', 120 ]],
+#              [['CS-TUA', 'LPPT LPPR', '0600']]]
+#[schedule] = [plane_name, Leg_flown, departure_time]
+
+#  initial state creation 
 ini_state = [[[x[0], None , None] for x in P],[0],L,[]]
 
-# funtion that adds times from strings in the format 'HHMM'
-# can be modified to include days, since at present it goes to '0000' after '2359'
 
+# funtion that adds times from strings in the format 
+# 'HHMM'+'HHMM' or 'ddHHMM'+'HHMM' then the output will be 
+# 'HHMM' or 'ddHHMM'+1day if it goes into day 2
+# exemple: '2200' + '0100' = '2300'
+#          '2200' + '0500' = '020300'
 def addtime(time1,time2):
-    import datetime as dt
-    HM1 = dt.time(int(time1[0:2]), int(time1[2:4]))
-    HM2 = dt.timedelta(hours = int(time2[0:2]),minutes = int(time2[2:4]))
-    HMadd = (dt.datetime.combine(dt.date(1,1,1),HM1) + HM2).time()
-    HM = HMadd.strftime('%H%M')
-    return HM
+    from datetime import datetime,timedelta
+    try:
+        T=int(time1[-6:-4])
+    except:
+        T=0  
+    HM1 = datetime(year=1, month=1, day=1+T, hour=int(time1[-4:-2]), minute= int(time1[-2:]))
+    HM2 = timedelta(hours=int(time2[-4:-2]),minutes=int(time2[-2:]))
+    DDadd = (HM1 + HM2)
+    DHM = DDadd.strftime('%d%H%M')
+    if DHM[-6:-4] == '01':
+        res = DDadd.strftime('%H%M')
+    else:
+        res = DDadd.strftime('%d%H%M')
+    return res
 
 # actions function
 # update1 so it only lets planes fly legs that start from their location
 # update2 so it doesnt let planes takeoff or land after those airports close
-# additional changes are needed to detect day changes in addtime function!!!
+# update3 with the new version of addtime it should detect day changes
 def actions(s):
 
     moves = []
@@ -92,26 +110,28 @@ def actions(s):
 
 
 # results function
-
+# update1 changed to make use of function addtime
 def results(s, a):
-    
-    import datetime as dt
     import copy
-    
     L2=[]
     added_profit = 0
     model = []
+    #cycle to determine the action starting time »ini_time«
+    #and airplane model »model«
     for g in s[0]:
-           if g[0] == a[1]:
-               if g[2] == None:
+           if g[0] == a[1]:   
+               if g[2] == None:  #if the plane hasnt been used 
                    for e in A:
                        if a[0][0:4] == e[0]:
-                           ini_time = e[1]     
+                           ini_time = e[1]  #starting time is set to departing airport opening time   
                else:
-                   ini_time = g[2]
+                   ini_time = g[2] #else its the plane's time
                for i in P:
                   if g[0] == i[0]:
                       model = copy.deepcopy(i[1])
+    #cycle to create the new list of not yet flown Legs »L2« 
+    #and travel time »T_time«
+    #and use the airplane »model« to determine profit from current Leg »added_profit«
     for f in s[2]:
         if f[0] != a[0]:
             L2.append(f)
@@ -120,31 +140,29 @@ def results(s, a):
             for i in range(0,len(f)):
                 if f[i] == model: 
                     added_profit = f[i+1]
+    #airplane rotation time »R_time« is determined from the »model«
     for i in C:
         if i[0] == model:
             R_time =copy.deepcopy(i[1])
-    e = int(ini_time[0:2])
-    f = int(ini_time[2:4])                 
-    d = int(T_time[0:2])
-    b = int(T_time[2:4])
-    c = int(R_time[0:2])
-    g = int(R_time[2:4])
-    result_state = []
-    HH = dt.time(e, f)
-    LH = dt.timedelta(hours = d,minutes = b)
-    CH = dt.timedelta(hours = c,minutes = g)
-    DH = (LH + CH)
-    H = (dt.datetime.combine(dt.date(1,1,1),HH) + DH).time()
+    #new plane time of day »ITR« is calculated from »ini_time« , »T_time« and »R_time«          
+    IT = addtime(ini_time,T_time)
+    ITR = addtime(IT,R_time)
+    #new plane status is created
+    #[plane_code, plane_location, plane_time]
     new_plane = []
     new_plane.append(a[1])
     new_plane.append(a[0][5:9])
-    new_plane.append(H.strftime('%H%M'))
+    new_plane.append(ITR)
     new_planeS = []
+    #this new plane status added with the other planes unchanged status
     for i in s[0]:
         if i[0] == new_plane[0]:
             new_planeS.append(new_plane)
         else:
             new_planeS.append(i)
+    #the new state is now assembled
+    #[Planes_Status, Profit_So_far, Not_Yet_Flown_Legs, Schedule]
+    result_state = []
     result_state.append(new_planeS)
     result_state.append([s[1][0]+added_profit])
     result_state.append(L2)
